db863ce93e17c022fb06a1739aa2b871013211be
Gladstone Rose <gladstonerose@hotmail.com>
2024-09-06 20:44:47 -0500

    first commit

---
+++ memory-jog.md
@@ -0,0 +1,4 @@
+link to source folder:
+
+
+/c/Users/glads/Downloads/Apply-a-Component-in-a-Render-Function/apply-component-in-render-function
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ProfilePage from "./components/ProfilePage";
+
+function App() {
+  return <ProfilePage />;
+}
+
+export default App;
